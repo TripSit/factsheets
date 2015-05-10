e1cb7d1514f15203c20e526015e80b8c216409e3
reality <tinmachin3@gmail.com>
2015-05-10 20:26:16 +0000

    boom, baby

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,32 @@
 var express = require('express');
+var _ = require('underscore')._;
 var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  request.get('http://tripbot.tripsit.me/api/tripsit/getAllDrugs', {
+    'json': true
+  }, function(request, response, body) {
+      res.render('index', { title: 'TripSit Factsheets', 'drugs': body.data[0] });
+  });
+});
+
+router.get('/factsheet/:name', function(req, res) {
+  request.get('http://tripbot.tripsit.me/api/tripsit/getDrug?name=' + req.params.name, {
+    'json': true
+  }, function(request, response, body) {
+      var order = _.union(['summary', 'dose', 'onset', 'duration'], _.keys(body.data[0]));
+      res.render('factsheet', { title: 'TripSit Factsheets - ' + body.data[0].name, 'drug': body.data[0], 'order': order });
+  });
+});
+
+router.get('/raw/:name', function(req, res) {
+  request.get('http://tripbot.tripsit.me/api/tripsit/getDrug?name=' + req.params.name, {
+    'json': true
+  }, function(request, response, body) {
+      res.json(body.data[0]);
+  });
 });
 
 module.exports = router;
