e821b53fa1df7d1fe05dc7fa05bb5bfc060262eb
reality <tinmachin3@gmail.com>
2015-05-14 18:18:07 +0000

    charts globbing

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,196 +1,196 @@
 window.onload = function () {
-    var name = $('#drugname').text()
-  $.getJSON('/raw/'+name, function(results) {
-      if(results.formatted_dose) {
-        $.each(results.formatted_dose, function(roa, levels) {
-          var dataPoints = [];
-          var a = 1;
-          $.each(levels, function(level, amt) {
-            amt = amt.replace(/mg/i,'').replace(/g/i,'').replace(/ug/i,'');
-            dataPoints.push({y: a, x: parseInt(amt)});
-            a++;
-          });
-          console.log(dataPoints);
-          var chart = new CanvasJS.Chart(roa+'Chart', {
-              theme: "theme2",
-              title:{
-                text: "Dosage response curve for " + roa + " " + name
-              },
-              animationEnabled: true,
-              axisX: {
-                title: 'Dosage'
-              },
-              axisY:{
-                title: 'Level'
-              },
-              data: [
-                  {        
-                    type: "spline",
-                    //lineThickness: 3,        
-                    dataPoints: dataPoints
-                  }
-              ]
-          });
-          chart.render();
-        });
+  if(drug.formatted_dose) {
+    $.each(drug.formatted_dose, function(roa, levels) {
+      var dataPoints = [];
+      var a = 1;
+      $.each(levels, function(level, amt) {
+        amt = amt.replace(/mg/i,'').replace(/g/i,'').replace(/ug/i,'');
+        dataPoints.push({y: a, x: parseInt(amt)});
+        a++;
+      });
+
+      var title = "Dosage response curve for " + roa + " " + drug.pretty_name;
+      if(roa == 'none') {
+        title = "Dosage response curve for " + drug.pretty_name;
+      } 
+
+      var chart = new CanvasJS.Chart(roa+'Chart', {
+          'theme': "theme2",
+          'title': { 
+            'text': title
+          },
+          'animationEnabled': true,
+          axisX: {
+            title: 'Dosage'
+          },
+          axisY:{
+            title: 'Level'
+          },
+          data: [
+              {        
+                type: "spline",
+                //lineThickness: 3,        
+                dataPoints: dataPoints
+              }
+          ]
+      });
+      chart.render();
+    });
+  }
+  
+  var data = [];
+  if(drug.formatted_duration.value && drug.formatted_onset.value && drug.formatted_aftereffects.value) {
+    var onset = parseInt(drug.formatted_onset.value);
+    if(drug.formatted_onset._unit == 'minutes') {
+      onset = onset / 60;
+    }
+    onset = [{'y': onset, 'label': 'All ROAs'}];
+    
+    var duration = parseInt(drug.formatted_duration.value);
+    if(drug.formatted_duration._unit == 'minutes') {
+      duration = duration / 60;
+    }
+    duration = [{'y': duration, 'label': 'All ROAs'}];
+
+    var after = parseInt(drug.formatted_aftereffects.value);
+    if(drug.formatted_aftereffects._unit == 'minutes') {
+      after = after / 60;
+    }
+    after = [{'y': after, 'label': 'All ROAs'}];
+
+    data.push({     
+      type: "stackedBar",
+      showInLegend: true,
+      name: "Onset",
+      axisYType: "secondary",
+      color: "#7E8F74",
+      dataPoints: onset
+    });
+    data.push({     
+        type: "stackedBar",
+        showInLegend: true,
+        name: "Duration",
+        axisYType: "secondary",
+        color: "#F0E6A7",
+        dataPoints: duration
+    }); 
+    data.push({     
+        type: "stackedBar",
+        showInLegend: true,
+        name: "After effects",
+        axisYType: "secondary",
+        color: "#ADD8E6",
+        dataPoints: after
+    });
+  } else {
+    var roas = []; 
+
+    // TODO : can actually loop this
+    // Onset
+    var dp = {
+      'onset': [],
+      'duration': [],
+      'aftereffects': []
+    };
+
+    $.each(drug.formatted_onset, function(roa, value) {
+      if(roa == '_unit' || roa == 'value') return;
+      if($.inArray(roa, roas) == -1) roas.push(roa);
+
+      var val = parseInt(value);
+      if(drug.formatted_onset._unit == 'minutes') {
+        val = val / 60;
       }
-        var data = [];
-      if(results.formatted_duration.value && results.formatted_onset.value && results.formatted_aftereffects.value) {
-        var onset = parseInt(results.formatted_onset.value);
-        if(results.formatted_onset._unit == 'minutes') {
-          onset = onset / 60;
-        }
-        onset = [{'y': onset, 'label': 'All ROAs'}];
-        
-        var duration = parseInt(results.formatted_duration.value);
-        if(results.formatted_duration._unit == 'minutes') {
-          duration = duration / 60;
-        }
-        duration = [{'y': duration, 'label': 'All ROAs'}];
+      dp.onset.push({'y': val, 'label': roa}) 
+    });
+    
+    // Duration
+    $.each(drug.formatted_duration, function(roa, value) {
+      if(roa == '_unit' || roa == 'value') return;
+      if($.inArray(roa, roas) == -1) roas.push(roa);
 
-        var after = parseInt(results.formatted_aftereffects.value);
-        if(results.formatted_aftereffects._unit == 'minutes') {
-          after = after / 60;
-        }
-        after = [{'y': after, 'label': 'All ROAs'}];
-
-        data.push({     
-          type: "stackedBar",
-          showInLegend: true,
-          name: "Onset",
-          axisYType: "secondary",
-          color: "#7E8F74",
-          dataPoints: onset
-        });
-        data.push({     
-            type: "stackedBar",
-            showInLegend: true,
-            name: "Duration",
-            axisYType: "secondary",
-            color: "#F0E6A7",
-            dataPoints: duration
-        }); 
-        data.push({     
-            type: "stackedBar",
-            showInLegend: true,
-            name: "After effects",
-            axisYType: "secondary",
-            color: "#ADD8E6",
-            dataPoints: after
-        });
-      } else {
-        var roas = []; 
-
-        // TODO : can actually loop this
-        // Onset
-        var dp = {
-          'onset': [],
-          'duration': [],
-          'aftereffects': []
-        };
-
-        $.each(results.formatted_onset, function(roa, value) {
-          if(roa == '_unit' || roa == 'value') return;
-          if($.inArray(roa, roas) == -1) roas.push(roa);
-
-          var val = parseInt(value);
-          if(results.formatted_onset._unit == 'minutes') {
-            val = val / 60;
-          }
-          dp.onset.push({'y': val, 'label': roa}) 
-        });
-        
-        // Duration
-        $.each(results.formatted_duration, function(roa, value) {
-          if(roa == '_unit' || roa == 'value') return;
-          if($.inArray(roa, roas) == -1) roas.push(roa);
-
-          var val = parseInt(value);
-          if(results.formatted_duration._unit == 'minutes') {
-            val = val / 60;
-          }
-          dp.duration.push({'y': val, 'label': roa}) 
-        });
-
-        // After effects
-        $.each(results.formatted_aftereffects, function(roa, value) {
-          if(roa == '_unit' || roa == 'value') return;
-          if($.inArray(roa, roas) == -1) roas.push(roa);
-
-          var val = parseInt(value);
-          if(results.formatted_aftereffects._unit == 'minutes') {
-            val = val / 60;
-          }
-          dp.aftereffects.push({'y': val, 'label': roa}) 
-        });
-
-        $.each(['onset','duration','aftereffects'], function(a, c) {
-          var s = results['formatted_'+c];
-          if(s.value) {
-            var val = parseInt(s.value);
-            if(s._unit == 'minutes') {
-              val = val / 60;
-            }
-            for(var i=0;i<roas.length;i++) {
-              dp[c].push({'y': val, 'label':roas[i]});  
-            }
-          }
-        });
-        console.log(dp);
-
-        data.push({     
-          type: "stackedBar",
-          showInLegend: true,
-          name: "Onset",
-          axisYType: "secondary",
-          color: "#7E8F74",
-          dataPoints: dp.onset
-        });
-        data.push({     
-            type: "stackedBar",
-            showInLegend: true,
-            name: "Duration",
-            axisYType: "secondary",
-            color: "#F0E6A7",
-            dataPoints: dp.duration
-        }); 
-        data.push({     
-            type: "stackedBar",
-            showInLegend: true,
-            name: "After effects",
-            axisYType: "secondary",
-            color: "#ADD8E6",
-            dataPoints: dp.aftereffects
-        });
+      var val = parseInt(value);
+      if(drug.formatted_duration._unit == 'minutes') {
+        val = val / 60;
       }
-      var chart = new CanvasJS.Chart("durationChart", {
-                title:{
-                        text:name + " duration"
-                },
-                animationEnabled: true,
-                axisX:{
-                        interval: 1,
-                        labelFontSize: 10,
-                        lineThickness: 0
-                },
-                axisY2:{
-                        valueFormatString: "0 hours",
-                        lineThickness: 0                                
-                },
-                toolTip: {
-                        shared: true
-                },
-                legend:{
-                        verticalAlign: "top",
-                        horizontalAlign: "center"
-                },
+      dp.duration.push({'y': val, 'label': roa}) 
+    });
 
-                data: data 
-        });
-        chart.render();
+    // After effects
+    $.each(drug.formatted_aftereffects, function(roa, value) {
+      if(roa == '_unit' || roa == 'value') return;
+      if($.inArray(roa, roas) == -1) roas.push(roa);
 
+      var val = parseInt(value);
+      if(drug.formatted_aftereffects._unit == 'minutes') {
+        val = val / 60;
+      }
+      dp.aftereffects.push({'y': val, 'label': roa}) 
+    });
 
-  });
+    /*$.each(['onset','duration','aftereffects'], function(a, c) {
+      var s = drug['formatted_'+c];
+      if(s.value) {
+        var val = parseInt(s.value);
+        if(s._unit == 'minutes') {
+          val = val / 60;
+        }
+        for(var i=0;i<roas.length;i++) {
+          dp[c].push({'y': val, 'label':roas[i]});  
+        }
+      }
+    });*/
+
+    data.push({     
+      type: "stackedBar",
+      showInLegend: true,
+      name: "Onset",
+      axisYType: "secondary",
+      color: "#7E8F74",
+      dataPoints: dp.onset
+    });
+    data.push({     
+        type: "stackedBar",
+        showInLegend: true,
+        name: "Duration",
+        axisYType: "secondary",
+        color: "#F0E6A7",
+        dataPoints: dp.duration
+    }); 
+    data.push({     
+        type: "stackedBar",
+        showInLegend: true,
+        name: "After effects",
+        axisYType: "secondary",
+        color: "#ADD8E6",
+        dataPoints: dp.aftereffects
+    });
+  }
+  var chart = new CanvasJS.Chart("durationChart", {
+            title:{
+                    text:name + " duration"
+            },
+            animationEnabled: true,
+            axisX:{
+                    interval: 1,
+                    labelFontSize: 10,
+                    lineThickness: 0
+            },
+            axisY2:{
+                    valueFormatString: "0 hours",
+                    lineThickness: 0                                
+            },
+            toolTip: {
+                    shared: true
+            },
+            legend:{
+                    verticalAlign: "top",
+                    horizontalAlign: "center"
+            },
+
+            data: data 
+    });
+    chart.render();
 
 var bt = $('.sidebar').position().top;
 
--- routes/index.js
+++ routes/index.js
@@ -62,35 +62,71 @@ router.get('/factsheet/:name', function(req, res) {
   request.get('http://tripbot.tripsit.me/api/tripsit/getDrug?name=' + req.params.name, {
     'json': true
   }, function(request, response, body) {
-      _.each(_.keys(glossary), function(item) {
-        body.data[0].properties.summary = body.data[0].properties.summary.replace(new RegExp(item, 'gi'), '<span style="color:#50007F;" data-toggle="tooltip", title="'+glossary[item]+'">'+item+'</span>');
-      });
-      _.each(_.keys(drugCache), function(item) {
-        body.data[0].properties.summary = body.data[0].properties.summary.replace(new RegExp(' '+item+' ', 'gi'), ' <a href="/factsheet/'+item+'">'+item+'</a> ');
-      });
-      var order = _.union(['summary', 'categories', 'dose', 'onset', 'duration', 'after-effects', 'effects'], _.keys(body.data[0].properties));
+    var drug = body.data[0];
+    _.each(_.keys(glossary), function(item) {
+      drug.properties.summary = drug.properties.summary.replace(new RegExp(item, 'gi'), '['+item+']');
+    });
+    var terms = /\[([^\]]+)\]/gi;
+    var item = terms.exec(drug.properties.summary);
+    while(item != null) {
+        drug.properties.summary = drug.properties.summary.replace(item[0], '<span style="color:#50007F;" data-toggle="tooltip", title="'+glossary[item[1]]+'">'+item[1]+'</span>');
+        item = terms.exec(drug.properties.summary);
+    }
+    _.each(_.keys(drugCache), function(item) {
+      drug.properties.summary = drug.properties.summary.replace(new RegExp(' '+item+' ', 'gi'), ' <a href="/factsheet/'+item+'">'+item+'</a> ');
+    });
+    var order = _.union(['summary', 'categories', 'dose', 'onset', 'duration', 'after-effects', 'effects'], _.keys(drug.properties));
 
-      var safety = {
-        'deadly': [],
-        'unsafe': [],
-        'safeinc': [],
-        'safedec': []
-      };
-      if(_.has(combos, body.data[0].name)) {
-        _.each(combos[body.data[0].name], function(d,k) {
-          if(d == 'Safe & Synergy') {
-              safety.safeinc.push(k); 
-          } else if(d == 'Safe & No Synergy') {
-              safety.safedec.push(k);
-          } else if(d == 'Deadly') {
-              safety.deadly.push(k);
-          } else if(d == 'Unsafe') {
-              safety.unsafe.push(k);
+    var safety = {
+      'deadly': [],
+      'unsafe': [],
+      'safeinc': [],
+      'safedec': []
+    };
+    if(_.has(combos, drug.name)) {
+      _.each(combos[drug.name], function(d,k) {
+        if(d == 'Safe & Synergy') {
+            safety.safeinc.push(k); 
+        } else if(d == 'Safe & No Synergy') {
+            safety.safedec.push(k);
+        } else if(d == 'Deadly') {
+            safety.deadly.push(k);
+        } else if(d == 'Unsafe') {
+            safety.unsafe.push(k);
+        }
+      }); 
+    }
+
+    // This is a little bit grea-hea-heasy, but y'know.
+    if((drug.formatted_duration && drug.formatted_onset && drug.formatted_aftereffects) &&
+      (_.size(drug.formatted_duration) > 1 || _.size(drug.formatted_onset) > 1 || _.size(drug.formatted_aftereffects) > 1)) {
+
+
+
+      var roas = [];
+      _.each(['onset', 'duration', 'aftereffects'], function(a, c) {
+        var s = drug['formatted_'+a];
+      console.log(s);
+        roas = _.union(roas, _.without(_.keys(s), '_unit', 'value'));
+      });
+
+      console.log(roas);
+
+      _.each(['onset', 'duration', 'aftereffects'], function(a, c) {
+        var s = drug['formatted_'+a];
+        console.log('now processing ' + a);
+        console.log(s);
+        _.each(roas, function(roa) {
+          if(!_.has(s, roa)) {
+          console.log('adding ' + s.value + ' to ' + a + ' as ' + roa);
+            s[roa] = s.value;
           }
-        }); 
-      }
+        });
+      });
+    }
 
-      res.render('factsheet', { title: 'TripSit Factsheets - ' + body.data[0].pretty_name, 'drug': body.data[0], 'order': order, 'glossary': glossary, 'interactions': safety });
+
+      res.render('factsheet', { title: 'TripSit Factsheets - ' + drug.pretty_name, 'drug': drug, 'order': order, 'glossary': glossary, 'interactions': safety });
   });
 });
 
@@ -103,3 +139,7 @@ router.get('/raw/:name', function(req, res) {
 });
 
 module.exports = router;
+
+function escapeRegExp(string){
+  return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
