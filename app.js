2e5b12c4ecaa99fe6f69d0511fbf305a28abff44
reality <tinmachin3@gmail.com>
2016-03-08 15:53:33 +0000

    better compression

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
+app.use(compression({'level': 6}));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(compression({'level': 6}));
 app.use(minify({ 'js_match': /javascripts/ }));
 
 app.use('/', routes);
