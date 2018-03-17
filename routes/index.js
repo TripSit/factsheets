498c18401e3f88a72b0ead8241ac341727b82f3b
Bjørn <30187002+Spacebjorn@users.noreply.github.com>
2018-03-17 14:57:54 +0100

    replaced nourishedcloud with tripsit.me
     for the combo cache get on line 174 as nourishedcloud is dead.

--- routes/index.js
+++ routes/index.js
@@ -172,7 +172,7 @@ setInterval(updateErowidCache, 3600000);
 updateErowidCache();
 
 // Grab the combos (note: this is not auto updated)
-request.get('http://nourishedcloud.com/combo_beta.json', {
+request.get('https://tripsit.me/combo_beta.json', {
   'json': true
 }, function(request, response, body) {
   combos = body;
