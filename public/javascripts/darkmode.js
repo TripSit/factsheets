3389de3f716d01e5d48ed7263b9f0671f3c0fd44
Bjørn <bjorn@tripsit.me>
2018-10-31 22:27:04 +0100

    moved css out of head. adjusted theme switcher to work with it

--- public/javascripts/darkmode.js
+++ public/javascripts/darkmode.js
@@ -4,50 +4,38 @@
 function changeCss() {
 	if($( "#switch" ).prop( "checked")) {
 		localStorage.setItem("loadout", "dark")
-		//localStorage.setItem("bootstrap", "dark")
-
-        //document.getElementById("bootstrap-theme").href = "/stylesheets/bootstrap-dark.min.css";
-        $('head').append('<link rel="stylesheet" type="text/css" href="/stylesheets/dark.css">')
-
+		document.getElementById("theme").href = "/stylesheets/dark.css";
 		drawCharts('dark');
 	}
 	else{
 		localStorage.setItem("loadout", "light")
-		//localStorage.setItem("bootstrap", "light")
-
-        $('link[rel=stylesheet][href*="dark.css"]').remove();
-        //document.getElementById("bootstrap-theme").href = "/stylesheets/bootstrap-light.min.css";
+		document.getElementById("theme").href = "/stylesheets/light.css";
 		drawCharts('light');
 	}
 }
 
-//called when site launched or browser refreshed.
-/*$( document ).ready( loadStyle );*/
+
+
 function loadStyle() {
 	var stylePreference = localStorage.getItem("loadout");
 	if(stylePreference) {
 		if(stylePreference === "dark") {
-            $('head').append('<link rel="stylesheet" type="text/css" href="/stylesheets/dark.css">')
-			//Check the switch button is set correctly
+            document.getElementById("theme").href = "/stylesheets/dark.css";
 			$( "#switch" ).prop( "checked", true );
-			//Draws the dark graphs
 			drawCharts('dark');
 		}
 		else {
-			//Check the switch button is set correctly
 			$("#switch").prop( "checked", false );
-            $('head').append('<link rel="stylesheet" type="text/css" href="/stylesheets/light.css">')
-			//Draws the light graphs
+            document.getElementById("theme").href = "/stylesheets/light.css";
 			drawCharts('light');
 		}
 	//Incase there's no localStorage draws the light graph	
 	} else {
-        $('head').append('<link rel="stylesheet" type="text/css" href="/stylesheets/light.css">')
+        document.getElementById("theme").href = "/stylesheets/light.css";
 		drawCharts('light');
 	}
 };
 
-//loads a screen to cover the changing of themes and accidental blinding
 $(window).on('load', function () {
 	$("#coverScreen").hide();
 	loadStyle();
