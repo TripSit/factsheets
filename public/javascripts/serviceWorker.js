66144d8745613f6585760432c3bd2412647b8988
Bjørn <bjorn@tripsit.me>
2018-11-02 21:37:44 +0100

    Added service workers. Shit is now chached bby. still needs some sort of version solution

---
+++ public/javascripts/serviceWorker.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('../sw_cached_site.js')
+        .then(reg => console.log('Service Worker: Registered (Pages)'))
+        .catch(err => console.log(`Service Worker: Error: ${err}`));
+    });
+  }
